ce50d5e993da335ad3917ec5aeda627974180d25
y18513569330 <308130358@qq.com>
2020-07-07 11:37:16 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -2,33 +2,33 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-const Home = () => import("../views/home/Home")
-const Category = () => import("../views/category/Category")
-const Cart = () => import("../views/cart/Cart")
-const Proflie = () => import("../views/profile/Profile")
+// const Home = () => import("../views/home/Home")
+// const Category = () => import("../views/category/Category")
+// const Cart = () => import("../views/cart/Cart")
+// const Proflie = () => import("../views/profile/Profile")
 
 
 export default new Router({
   routes: [
-    {
-      path:'',
-      redirect:"/home"
-    },
-    {
-      path:"/home",//首页
-      component:Home
-    },
-    {
-      path:"/category",//分类
-      component:Category
-    },
-    {
-      path:"/cart",//购物车
-      component:Cart
-    },
-    {
-      path:"/profile",// 我的
-      component:Proflie
-    }
+  //   {
+  //     path:'',
+  //     redirect:"/home"
+  //   },
+  //   {
+  //     path:"/home",//首页
+  //     component:Home
+  //   },
+  //   {
+  //     path:"/category",//分类
+  //     component:Category
+  //   },
+  //   {
+  //     path:"/cart",//购物车
+  //     component:Cart
+  //   },
+  //   {
+  //     path:"/profile",// 我的
+  //     component:Proflie
+  //   }
   ]
 })
