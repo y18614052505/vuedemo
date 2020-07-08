34b43d162865fa1d8cd143b7619cfacddb494f1a
y18513569330 <308130358@qq.com>
2020-07-08 10:10:35 +0800

    添加公共的navbar.vue文件

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar">
+        <div class='left'><slot name='left'></slot></div>
+        <div class='center'><slot name='center'></slot></div>
+        <div class='right'><slot name='right'></slot></div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"navbar"
+}
+</script>
+
+<style  scope>
+    .navbar{
+        /*css3 看下display*/
+        display:flex;
+        height:44px;
+        line-height: 44px;
+        text-align: center;
+        box-shadow: 0 1px 1px rgba(100,100,100,.2);
+    }
+    .left,.right{
+        width:40px;
+    }
+    .center{
+        flex:1;
+        /* background-color:red */
+    }
+</style>
