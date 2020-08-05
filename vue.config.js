2063e38da585348f5b65c67c9b66396dbcdb0805
y18513569330 <308130358@qq.com>
2020-08-05 16:40:20 +0800

    没有修改

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,19 @@ module.exports={
                 "network":"@/network",
                 "common":"@/common"//  common指的是公共的js文件夹
             }
+        },
+        //对服务运行添加代理服务
+        devServer:{
+            proxy:{
+                '/api':{
+                    target:'http://pv.sohu.com',//接口的域名,注意这里是域名，不是完整的ip;
+                    changeOrigin:true,// 如果是https接口，需要配置这个参数;
+                    ws:true,
+                    pathRewrite:{
+                        '^/api':''
+                    }
+                }
+            }
         }
     }
 }
